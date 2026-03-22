4b113cea7fbfd5a98f8cddd8fb7da374c0dd1ba6
benisai <isai.beniamin@gmail.com>
2026-03-22 15:10:36 -0700

    dashboard: fix network activity hover tooltip rate formatter

--- moci/js/modules/dashboard.js
+++ moci/js/modules/dashboard.js
@@ -567,8 +567,8 @@ export default class DashboardModule {
 
 		this.bandwidthTooltip.innerHTML = `
 			<div class="bandwidth-tooltip-title">${this.core.escapeHtml(when)}</div>
-			<div>Download: ${this.core.escapeHtml(this.core.formatSpeed(down))}</div>
-			<div>Upload: ${this.core.escapeHtml(this.core.formatSpeed(up))}</div>
+			<div>Download: ${this.core.escapeHtml(this.core.formatRate(down))}</div>
+			<div>Upload: ${this.core.escapeHtml(this.core.formatRate(up))}</div>
 			<div>Sample: ${index + 1}/${this.bandwidthHistory.down.length}</div>
 		`;
 
